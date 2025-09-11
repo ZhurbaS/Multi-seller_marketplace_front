62639f1b65f6cb79c1b7a13bc22f1759bae012b3
Sergij <sl.zhurba1@gmail.com>
2025-09-11 11:54:05 +0300

    Chat seller-customer added

--- src/store/reducers/chatSlice.js
+++ src/store/reducers/chatSlice.js
@@ -5,12 +5,34 @@ export const add_friend = createAsyncThunk(
   "chat/add_friend",
   async (info, { rejectWithValue, fulfillWithValue }) => {
     try {
-      const { data } = await api.post("/chat/customer/add-customer-friend", info);
-      
+      const { data } = await api.post(
+        "/chat/customer/add-customer-friend",
+        info
+      );
+
       //   console.log(data);
       return fulfillWithValue(data);
     } catch (error) {
       console.error("💥 Error in chatSlice: add_friend:", error);
+      //   console.log(error.response);
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const send_message = createAsyncThunk(
+  "chat/send_message",
+  async (info, { rejectWithValue, fulfillWithValue }) => {
+    try {
+      const { data } = await api.post(
+        "/chat/customer/send-message-to-seller",
+        info
+      );
+
+      // console.log(data);
+      return fulfillWithValue(data);
+    } catch (error) {
+      console.error("💥 Error in chatSlice: send_message:", error);
       //   console.log(error.response);
       return rejectWithValue(error.response.data);
     }
@@ -33,13 +55,28 @@ export const chatSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    // builder
-    //   .addCase(customer_login.fulfilled, (state, { payload }) => {
-    //     const userInfo = decodeToken(payload.token);
-    //     state.loader = false;
-    //     state.successMessage = payload.message;
-    //     state.userInfo = userInfo;
-    //   });
+    builder
+      .addCase(add_friend.fulfilled, (state, { payload }) => {
+        state.fd_messages = payload.messages;
+        state.currentFd = payload.currentFd;
+        state.my_friends = payload.MyFriends;
+      })
+      .addCase(send_message.fulfilled, (state, { payload }) => {
+        let tempFriends = state.my_friends;
+        let index = tempFriends.findIndex(
+          (f) => f.fdId === payload.message.receiverId
+        );
+        while (index > 0) {
+          let temp = tempFriends[index];
+          tempFriends[index] = tempFriends[index - 1];
+          tempFriends[index - 1] = temp;
+          index--;
+        }
+
+        state.my_friends = tempFriends;
+        state.fd_messages = [...state.fd_messages, payload.message];
+        state.successMessage = "Message sent successfully";
+      });
   },
 });
 
--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,6 +1,6 @@
 import authSlice from "./reducers/authSlice";
 import cardSlice from "./reducers/cardSlice";
-import { chatSlice } from "./reducers/chatSlice";
+import chatSlice from "./reducers/chatSlice";
 import dashboardSlice from "./reducers/dashboardSlice";
 import homeSlice from "./reducers/homeSlice";
 import orderSlice from "./reducers/orderSlice";
